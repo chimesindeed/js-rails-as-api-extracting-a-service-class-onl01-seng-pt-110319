368aee7282e33041e50b8e6c13bee6cf65bd7aae
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 08:27:55 +0000

    Done.

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,9 +5,16 @@ class SightingSerializer
   end
   
   def to_serialized_json
-    @sighting.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
+    options = {
+      include: {
+        bird: {
+          only: [:name, :species]
+        },
+        location: {
+          only: [:latitude, :longitude]
+        }
+    }, 
+    :except: [:updated_at],
+    }
   end
 end
